11b233b62d3f8e8cd687e4bc29130b5cfe5a204f
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-08-20 13:04:25 +0900

    Fix: find files deeper

--- index.ts
+++ index.ts
@@ -1,76 +1,93 @@
-const filePath = Deno.args[0];
+const folder = Deno.args[0];
 
-const file = await Deno.readTextFile(filePath);
+const filePaths: string[] = [];
 
-const lines = file.split("\n");
-
-const matches = {
-  notify: {
-    match: /root\.\$notify/g,
-    count: 0,
-    to: "Notification",
-  },
-  router: {
-    match: /root\.\$router/g,
-    count: 0,
-    to: "router",
-  },
-  route: {
-    match: /root\.\$route/g,
-    count: 0,
-    to: "route",
-  },
+const findFilesDeep = (folder: string) => {
+  const files = Deno.readDirSync(folder);
+  for (const file of files) {
+    if (file.isFile) {
+      filePaths.push(file.name);
+    } else {
+      findFilesDeep(file.name);
+    }
+  }
 };
 
-const convertLine = (line: string) => {
-  Object.keys(matches).forEach((key) => {
-    const selectedMatches = matches[key as keyof typeof matches];
-    const match = selectedMatches.match.exec(line);
-    if (match) selectedMatches.count++;
-    line = line.replace(selectedMatches.match, selectedMatches.to);
-  });
-  return line;
-};
+findFilesDeep(folder);
 
-const convertedLines = lines.map(convertLine);
+filePaths.forEach(async (filePath) => {
+  const file = await Deno.readTextFile(filePath);
 
-const scriptTagStartIndex = () =>
-  convertedLines.findIndex((line) => line.match(/<script.*>/));
-const setupFunctionStartIndex = () =>
-  convertedLines.findIndex((line) => line.match(/setup\(.*\)/));
+  const lines = file.split("\n");
 
-if (matches.notify.count > 0) {
-  convertedLines.splice(
-    scriptTagStartIndex() + 1,
-    0,
-    `import { Notification } from 'element-ui';`
-  );
-}
-if (matches.router.count > 0 || matches.route.count > 0) {
-  convertedLines.splice(
-    scriptTagStartIndex() + 1,
-    0,
-    `import { useRoute, useRouter } from 'vue-router'`
-  );
-}
-if (matches.router.count > 0) {
-  convertedLines.splice(
-    setupFunctionStartIndex() + 1,
-    0,
-    `const router = useRouter();`
-  );
-}
-if (matches.route.count > 0) {
-  convertedLines.splice(
-    setupFunctionStartIndex() + 1,
-    0,
-    `const route = useRoute();`
-  );
-}
+  const matches = {
+    notify: {
+      match: /root\.\$notify/g,
+      count: 0,
+      to: "Notification",
+    },
+    router: {
+      match: /root\.\$router/g,
+      count: 0,
+      to: "router",
+    },
+    route: {
+      match: /root\.\$route/g,
+      count: 0,
+      to: "route",
+    },
+  };
 
-convertedLines[setupFunctionStartIndex()] = convertedLines[
-  setupFunctionStartIndex()
-].replace(/root,?/g, "");
+  const convertLine = (line: string) => {
+    Object.keys(matches).forEach((key) => {
+      const selectedMatches = matches[key as keyof typeof matches];
+      const match = selectedMatches.match.exec(line);
+      if (match) selectedMatches.count++;
+      line = line.replace(selectedMatches.match, selectedMatches.to);
+    });
+    return line;
+  };
 
-const newFile = convertedLines.join("\n");
-await Deno.writeTextFile(filePath, newFile);
+  const convertedLines = lines.map(convertLine);
+
+  const scriptTagStartIndex = () =>
+    convertedLines.findIndex((line) => line.match(/<script.*>/));
+  const setupFunctionStartIndex = () =>
+    convertedLines.findIndex((line) => line.match(/setup\(.*\)/));
+
+  if (matches.notify.count > 0) {
+    convertedLines.splice(
+      scriptTagStartIndex() + 1,
+      0,
+      `import { Notification } from 'element-ui';`
+    );
+  }
+  if (matches.router.count > 0 || matches.route.count > 0) {
+    convertedLines.splice(
+      scriptTagStartIndex() + 1,
+      0,
+      `import { useRoute, useRouter } from 'vue-router'`
+    );
+  }
+  if (matches.router.count > 0) {
+    convertedLines.splice(
+      setupFunctionStartIndex() + 1,
+      0,
+      `const router = useRouter();`
+    );
+  }
+  if (matches.route.count > 0) {
+    convertedLines.splice(
+      setupFunctionStartIndex() + 1,
+      0,
+      `const route = useRoute();`
+    );
+  }
+
+  convertedLines[setupFunctionStartIndex()] = convertedLines[
+    setupFunctionStartIndex()
+  ].replace(/root,?/g, "");
+
+  const newFile = convertedLines.join("\n");
+  await Deno.writeTextFile(filePath, newFile);
+});
